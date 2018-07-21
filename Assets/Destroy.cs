7942ea143c87a59def82b5200374eb197e6ea534
JTshaka <javiertorresgutierrez@gmail.com>
2018-07-21 15:24:06 -0500

    Initial commit

---
+++ Assets/Destroy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+
+    public float spinValue = 90;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * spinValue * Time.deltaTime);
+    }
+
+    public void flipSpin() 
+    {
+        Destroy(gameObject);
+    }
+
+}
+      
+                         
